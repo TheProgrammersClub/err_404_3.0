0b5dc40572c55b1cd748cbda167abf7223396232
Jawad <shaikhjawad007@gmail.com>
2017-06-16 15:24:51 +0530

    trophy animation

--- scripts.js
+++ scripts.js
@@ -17,6 +17,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	});
 	$('.modal').modal();
-		
+
+
+
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+
+	function handleScroll(){
+		var badge = document.querySelector(".trophy-badge");
+		if (isElementInViewport(badge)){
+			badge.classList.add('badge-visible');
+		}
+	}
+	
+
+	window.addEventListener("scroll", handleScroll);
 	
 })
