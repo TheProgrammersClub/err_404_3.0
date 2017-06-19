82c7c6f023ac4ea281868cb1c0eff010acfb9476
Jawad <shaikhjawad007@gmail.com>
2017-06-19 16:30:36 +0530

    added scrollReveal

--- scripts.js
+++ scripts.js
@@ -37,37 +37,6 @@ $(document).ready( function () {
 		},
 		minLength: 0,
 	});
-
-
-	// animations
-
-	var side_gfx = document.querySelectorAll(".side-gfx");
-
-	function isElementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-		return (
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-		);
-	}
-
-	function handleScroll(){
-		var badge = document.querySelector(".trophy-badge");
-		if (isElementInViewport(badge)){
-			badge.classList.add('badge-visible');
-		}
-		for (var i = 0; i < side_gfx.length; i++) {
-			if (isElementInViewport(side_gfx[i])) {
-				side_gfx[i].classList.add("side-gfx-visible");
-			}
-		}
-	}
-	
-
-	// event listeners
-	window.addEventListener("scroll", handleScroll);
 	
 })
 
@@ -78,4 +47,12 @@ function loadProgress() {
 
 	document.getElementById('loader').style.display = "none";
 	document.getElementById('main').style.display = "block";
+
+	// animate the logo after loader is removed load
+	main_pop_up_options = {
+		duration: 800,
+		distance: '20px',
+		scale: 1,
+	};
+	sr.reveal(".main-logo", main_pop_up_options);
 }
