f8e97f55eea5cb03fc91efe689186f4b7056c3a7
adimyth <mishraaditya6991@gmail.com>
2017-10-14 17:52:53 +0530

    Winners

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ $(document).ready( function () {
 	// initializing side nav
 	// $(".button-collapse").sideNav();
 	$('.button-collapse').sideNav({
-      menuWidth: 300,
-      edge: 'left',
-      closeOnClick: true,
-      draggable: true
-    });
-    $(".drag-target").on("swipeleft", function () {
-      $("#sidenav-overlay").trigger("click");
-    });
+		menuWidth: 300,
+		edge: 'left',
+		closeOnClick: true,
+		draggable: true
+	});
+	$(".drag-target").on("swipeleft", function () {
+		$("#sidenav-overlay").trigger("click");
+	});
 
 	$(".scrollspy").scrollSpy({
 		scrollOffset:60
@@ -132,35 +132,35 @@ function countDownTimer(){
 function print_dino_say(){
 	// printing a dino-say kinda dialogue to the console (if you modify it, please review the placements of slashes before pushing)
 	dinoSay = "\n\
-	    __________________________________________________________________________\n\
-	   /																		   \\ \n\
-	  /   Hello there random stranger!                                              \\ \n\
-	 /    I think you are interested in this website's guts.                         \\ \n\
+	__________________________________________________________________________\n\
+	/																		   \\ \n\
+	/   Hello there random stranger!                                              \\ \n\
+	/    I think you are interested in this website's guts.                         \\ \n\
 	/     I'm the guardian of it's source code. RAAWR!                                \\ \n\
 	\\     Luckily, I believe in sharing with the community.                           /\n\
-	 \\    So here's the code : https://github.com/TheProgrammersClub/err_404         /\n\
-	  \\   Make wise use of it.                                                      /\n\
-	   \\ 																		   /\n\
-	    ---------------------------------------------------------------------------\n\
-				███████████       /\n\
-			  ████▄███████████   /\n\
-			  ████████████████  /\n\
-			  ████████████████ /\n\
-			  ████████\n\
-			  ████████████\n\
-█ 			  ██████\n\
-█ 			 ███████\n\
-███ 	   ████████████\n\
-█████   ████████████  █\n\
- ███████████████████\n\
-  ██████████████████\n\
-	 █████████████\n\
-	 ███████████\n\
-	  ████████\n\
-	  ████  ██\n\
-	  ███    █\n\
-	  █      █\n\
-	  ███    ███"
+	\\    So here's the code : https://github.com/TheProgrammersClub/err_404         /\n\
+	\\   Make wise use of it.                                                      /\n\
+	\\ 																		   /\n\
+	---------------------------------------------------------------------------\n\
+	███████████       /\n\
+	████▄███████████   /\n\
+	████████████████  /\n\
+	████████████████ /\n\
+	████████\n\
+	████████████\n\
+	█ 			  ██████\n\
+	█ 			 ███████\n\
+	███ 	   ████████████\n\
+	█████   ████████████  █\n\
+	███████████████████\n\
+	██████████████████\n\
+	█████████████\n\
+	███████████\n\
+	████████\n\
+	████  ██\n\
+	███    █\n\
+	█      █\n\
+	███    ███"
 	console.log(dinoSay);
 }
 
@@ -183,3 +183,8 @@ function loadProgress() {
 	};
 	sr.reveal(".main-logo", main_pop_up_options);
 }
+
+
+$(document).ready(function(){
+	$('.slider').slider();
+});
