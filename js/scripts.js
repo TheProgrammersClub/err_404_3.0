340df9f954b545fb3d11e75f1bb25600140d1c01
Praneet9 <praneetbomma@gmail.com>
2019-02-26 21:20:51 +0530

    almost done

--- js/scripts.js
+++ js/scripts.js
@@ -102,34 +102,34 @@ $(document).ready( function () {
 	// logging dino-say
 	//print_dino_say();
 
-	// setInterval(countDownTimer, 1000);
-	
+	setInterval(countDownTimer, 500)
 	
 });
 
 
-// function countDownTimer(){
-//
-// 	var event_date = new Date(2019, 01, 07, 9, 00, 00);
-// 	var curr_date = new Date();
-// 	var t1 = event_date.getTime();
-// 	var t2 = curr_date.getTime();
-// 	var day = Math.floor((t1-t2)/(24*60*60*1000));
-// 	t1 = t1 - day*24*60*60*1000;
-// 	var hour = Math.floor((t1-t2)/(60*60*1000));
-// 	t1 = t1 - hour*60*60*1000;
-// 	var min = Math.floor((t1-t2)/(60*1000));
-// 	t1 = t1 - min*60*1000;
-// 	var sec = Math.floor((t1-t2)/(1000));
-//
-// 	document.getElementById('day').innerHTML = day;
-// 	document.getElementById('hour').innerHTML = hour;
-// 	document.getElementById('min').innerHTML = min;
-// 	document.getElementById('sec').innerHTML = sec;
-//
-// 	// console.log(event_date + "\n" + curr_date + "\n" + diff);
-// 	// console.log(day + "\n" + hour + "\n" + min + "\n" + sec);
-// }
+function countDownTimer(){
+
+	var event_date = new Date(2019, 02, 16, 14, 00, 00);
+	var curr_date = new Date();
+	var t1 = event_date.getTime();
+	var t2 = curr_date.getTime();
+	
+	var day = Math.floor((t1-t2)/(24*60*60*1000));
+	t1 = t1 - day*24*60*60*1000;
+	var hour = Math.floor((t1-t2)/(60*60*1000));
+	t1 = t1 - hour*60*60*1000;
+	var min = Math.floor((t1-t2)/(60*1000));
+	t1 = t1 - min*60*1000;
+	var sec = Math.floor((t1-t2)/(1000));
+
+	document.getElementById('day').innerHTML = day;
+	document.getElementById('hour').innerHTML = hour;
+	document.getElementById('min').innerHTML = min;
+	document.getElementById('sec').innerHTML = sec;
+
+	// console.log(event_date + "\n" + curr_date + "\n" + diff);
+	// console.log(day + "\n" + hour + "\n" + min + "\n" + sec);
+}
 
 function print_dino_say(){
 	// printing a dino-say kinda dialogue to the console (if you modify it, please review the placements of slashes before pushing)
